4f5658aa1e2239fbabe92dc37f55f06f35d9f03c
haaragard <haaragard@gmail.com>
2024-12-29 16:37:55 -0300

    feat: add method create into UserRepository implementations

--- app/Infrastructure/Persistence/Mongodb/Repository/User/UserRepository.php
+++ app/Infrastructure/Persistence/Mongodb/Repository/User/UserRepository.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace app\Infrastructure\Persistence\Mongodb\Repository\User;
 
+use App\Domain\_Shared\Entity;
+use App\Domain\User\Entity\UserEntity;
 use App\Domain\User\Repository\UserRepositoryInterface;
 
 class UserRepository implements UserRepositoryInterface
 {
-
+    public function create(Entity|UserEntity $entity): void
+    {
+        // TODO: Implement create() method.
+    }
 }
--- app/Infrastructure/Persistence/Mysql/Repository/User/UserRepository.php
+++ app/Infrastructure/Persistence/Mysql/Repository/User/UserRepository.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Persistence\Mysql\Repository\User;
 
+use App\Domain\_Shared\Entity;
+use App\Domain\User\Entity\UserEntity;
 use App\Domain\User\Repository\UserRepositoryInterface;
 
 class UserRepository implements UserRepositoryInterface
 {
-
+    public function create(Entity|UserEntity $entity): void
+    {
+        // TODO: Implement create() method.
+    }
 }
