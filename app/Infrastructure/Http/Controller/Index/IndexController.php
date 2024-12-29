b8f0f9e999e6a72693c503521b66612d857c478f
haaragard <haaragard@gmail.com>
2024-12-29 16:37:11 -0300

    chore: update IndexController.php

--- app/Infrastructure/Http/Controller/Index/IndexController.php
+++ app/Infrastructure/Http/Controller/Index/IndexController.php
@@ -5,17 +5,19 @@ declare(strict_types=1);
 namespace App\Infrastructure\Http\Controller\Index;
 
 use App\Infrastructure\Http\Controller\_Shared\AbstractController;
+use Psr\Http\Message\ResponseInterface;
+use Swoole\Http\Status;
 
 class IndexController extends AbstractController
 {
-    public function index()
+    public function __invoke(): ResponseInterface
     {
         $user = $this->request->input('user', 'Hyperf');
         $method = $this->request->getMethod();
 
-        return [
+        return $this->response->json([
             'method' => $method,
             'message' => "Hello {$user}.",
-        ];
+        ])->withStatus(Status::OK);
     }
 }
